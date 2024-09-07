c0c60f49f07a717b96bd9650566adb50eef3b2b5
Brandon Darlington-Goddard <okeydokeygnocchi@gmail.com>
2024-09-07 13:19:54 -0700

    Add Invoke-WebRequest logic

--- Source/Public/Connect-Github.ps1
+++ Source/Public/Connect-Github.ps1
@@ -2,10 +2,19 @@ Param(
     [Parameter(Mandatory=$true)]
     [securestring]$PatToken,
     
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory=$false)]
     [string]$ServerUrl = "https://api.github.com",
 
     [Parameter(Mandatory=$false)]
     [ValidateSet("2022-11-28")]
     $ApiVersion = "2022-11-28"
 )
+
+$GetHeaders = @{
+    "Authorization" = "Bearer $($PatToken | ConvertFrom-SecureString -AsPlainText)"
+    "X-GitHub-Api-Version" = "$ApiVersion"
+}
+
+$Script = Invoke-WebRequest -Method Get -Uri $ServerUrl -Headers $GetHeaders
+
+Write-Host $Script
